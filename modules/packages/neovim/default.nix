e05b0a410b1239a90d55ecc68ca9f3357a858b83
RobertD95 <robertparson95@protonmail.com>
2026-02-19 11:24:29 -0800

    NVF
    Catppucchin + Transparent Background Added and works.

--- modules/packages/neovim/default.nix
+++ modules/packages/neovim/default.nix
@@ -11,7 +11,7 @@
           enable = true;
           name = "catppuccin";
           style = "mocha";
-          transparent_background = true;
+          transparent = true;
         };
         viAlias = true;
         vimAlias = true;
