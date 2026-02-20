02769bbfc0bcbbfb219295cea57a22212165e9aa
RobertD95 <robertparson95@protonmail.com>
2026-02-20 15:25:24 -0800

    NVF
    Telescope has its own nix file

--- modules/packages/neovim/default.nix
+++ modules/packages/neovim/default.nix
@@ -1,21 +1,20 @@
 { pkgs, ... }:
 {
   imports = [
-    # config.nix
+    telescope.nix
   ];
   programs.nvf = {
     enable = true;
     settings = {
       vim = {
+        viAlias = true;
+        vimAlias = true;
         theme = {
           enable = true;
           name = "catppuccin";
           style = "mocha";
           transparent = true;
         };
-        telescope.enable = true;
-        viAlias = true;
-        vimAlias = true;
         lsp = {
           enable = true;
         };
