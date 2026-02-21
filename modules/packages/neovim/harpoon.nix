cd4fed083b6f1f871d0a40f0e5308d8aa77bb655
RobertD95 <robertparson95@protonmail.com>
2026-02-20 19:03:54 -0800

    NVF
    Updated harpoon.nix

--- modules/packages/neovim/harpoon.nix
+++ modules/packages/neovim/harpoon.nix
@@ -2,5 +2,13 @@
 {
         programs.nvf.settings.vim.navigation.harpoon = {
                 enable = true;        
+                mappings = {
+                        markFile = "a";
+                        listMarks = "o";
+                        file1 = "<C-h>";
+                        file2 = "<C-j>";
+                        file3 = "<C-k>";
+                        file4 = "<c-l>";
+                };
         };
 }
