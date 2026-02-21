39d5a7fa5451e6eea7f1789aafdd15f7f399e4b7
RobertD95 <robertparson95@protonmail.com>
2026-02-20 20:27:05 -0800

    NVF
    Added Python lsp to neovim

--- modules/packages/neovim/lsp.nix
+++ modules/packages/neovim/lsp.nix
@@ -1,14 +1,21 @@
 { pkgs, ... }:
 {
-        programs.nvf.settings.vim = {
-                languages.nix = {
-                        enable = true;
-                        lsp.enable = true;
-                };
-                lsp = {
-                        enable = true;
-                        formatOnSave = true;
-                };
-                autocomplete.nvim-cmp.enable = true;
-        };
+  programs.nvf.settings.vim = {
+    languages = {
+      nix = {
+        enable = true;
+        lsp.enable = true;
+      };
+      python = {
+        enable = true;
+        lsp.enable = true;
+      };
+
+    };
+    lsp = {
+      enable = true;
+      formatOnSave = true;
+    };
+    autocomplete.nvim-cmp.enable = true;
+  };
 }
