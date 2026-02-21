4f467536037e800be14aa77d3808986a5b113757
RobertD95 <robertparson95@protonmail.com>
2026-02-20 19:23:35 -0800

    NVF
    Harpoon.nix new keybinds

--- modules/packages/neovim/harpoon.nix
+++ modules/packages/neovim/harpoon.nix
@@ -3,8 +3,8 @@
         programs.nvf.settings.vim.navigation.harpoon = {
                 enable = true;        
                 mappings = {
-                        markFile = "a";
-                        listMarks = "o";
+                        markFile = "m";
+                        listMarks = "l";
                         file1 = "<C-j>";
                         file2 = "<C-k>";
                         file3 = "<C-l>";
