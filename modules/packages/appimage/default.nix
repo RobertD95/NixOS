8a136bc2236b675f80e0709ed7096d419fa5944e
RobertD95 <robertparson95@protonmail.com>
2026-02-24 15:34:38 -0800

    Appimage
    Nothing Changed try something else later

--- modules/packages/appimage/default.nix
+++ modules/packages/appimage/default.nix
@@ -1,11 +1,13 @@
 { pkgs, lib, ... }:
 {
-  programs = {
-    appimage = {
-      # AppImage
-      enable = true;
-      binfmt = true;
-    };
-  };
+  #  programs = {
+  #    appimage = {
+  #      # AppImage
+  #      enable = true;
+  #      binfmt = true;
+  #    };
+  #  };
+  environment.systemPackages = with pkgs; [
+    appimageupdate
+  ];
 }
-
