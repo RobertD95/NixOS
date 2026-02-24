2de9f61fd8a055243009e792a8e17c3916caa58f
RobertD95 <robertparson95@protonmail.com>
2026-02-24 12:42:14 -0800

    APPIMAGE and Games Drive Update/Change

--- modules/packages/appimage/default.nix
+++ modules/packages/appimage/default.nix
@@ -1,10 +1,18 @@
 { pkgs, ... }:
 {
   programs = {
-    appimage = {	# AppImage
+    appimage = {
+      # AppImage
       enable = true;
       binfmt = true;
-      package = pkgs.appimage-run;
+      package = pkgs.appimage-run.override {
+        extraPKGS = pkgs: [
+          pkgs.libxshmfench
+          pkgs.xorg.libX11
+          pkgs.libGL
+        ];
+      };
     };
   };
 }
+
