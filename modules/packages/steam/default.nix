0d4b46eaebb8147c47fee454a12a2b755037deae
RobertD95 <robertparson95@protonmail.com>
2026-03-04 00:34:53 -0800

    Steam Update
    gamescope_git, mangohud_git added as an extra package for steam

--- modules/packages/steam/default.nix
+++ modules/packages/steam/default.nix
@@ -1,12 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
+let
+  nyx = inputs.nyx.legacyPackages.x86_64-linux;
+in
 {
   programs = {
     steam = {
       enable = true; # Gaming
       package = pkgs.steam.override {
         extraPkgs =
-          pkgs: with pkgs; [
-
+          pkgs: with nyx; [
+            mangohud_git
+            gamescope_git
           ];
       };
     };
