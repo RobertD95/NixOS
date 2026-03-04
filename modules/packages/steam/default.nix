34dc874711814268ca11652c2e575f38a6d6f8f7
RobertD95 <robertparson95@protonmail.com>
2026-03-04 13:54:19 -0800

    Update Steam
    removed mangohud and gamescope (causes crashing)

--- modules/packages/steam/default.nix
+++ modules/packages/steam/default.nix
@@ -1,18 +1,9 @@
 { pkgs, inputs, ... }:
-let
-  nyx = inputs.nyx.legacyPackages.x86_64-linux;
-in
 {
   programs = {
     steam = {
       enable = true; # Gaming
-      package = pkgs.steam.override {
-        extraPkgs =
-          pkgs: with nyx; [
-            mangohud_git
-            gamescope_git
-          ];
-      };
+      package = pkgs.steam;
     };
   };
 }
