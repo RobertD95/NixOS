5f8b5a414dca9bbb628be5d429ae0cf02fe31f24
RobertD95 <robertparson95@protonmail.com>
2026-03-04 20:25:03 -0800

    Hyprland v53.3-b
    Not the Latest but the most stable of the version before v54

--- flake.nix
+++ flake.nix
@@ -1,14 +1,24 @@
 {
-  inputs = {
-    pkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nix.url = "github:nixos/nixpkgs/nixos-unstable";
-    nyx.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-    nvf.url = "github:notashelf/nvf";
-#hyprland.url = "github:hyprwm/Hyprland";
-#hyprland-plugins = {
-#url = "github:hyprwm/hyprland-plugins";
-#inputs.hyprland.follows = "hyprland";
-#};
+        inputs = {
+                pkgs = {
+                        url = "github:nixos/nixpkgs/nixos-25.11";
+                };
+                nix = {
+                        url = "github:nixos/nixpkgs/nixos-unstable";
+                };
+                nyx = {
+                        url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+                };
+                nvf = {
+                        url = "github:notashelf/nvf";
+                };
+                hyprland = {
+                        url = "github:hyprwm/Hyprland/tree/v0.53.3-b";
+                };
+                hyprland-plugins = {
+                        url = "github:hyprwm/hyprland-plugins";
+                        inputs.hyprland.follows = "hyprland";
+                };
   };
   outputs =
     { self, nvf, ... }@inputs:
