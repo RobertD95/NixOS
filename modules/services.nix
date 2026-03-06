0247e7b8d7f31898ce4d78314508f92e67b920f6
RobertD95 <robertparson95@protonmail.com>
2026-03-06 02:07:42 -0800

    Update services.nix

--- modules/services.nix
+++ modules/services.nix
@@ -21,39 +21,38 @@
                 pulse.enable = true;
                 jack.enable = true;
                 #media-session.enable = true;
+services.pipewire.wireplumber.extraConfig."10-virtual-duplex.lua" = ''
+  -- Create virtual sink (output)
+  create_object("factory", {
+    ["factory.name"] = "support.null-audio-sink",
+    ["node.name"] = "Virtual-Cable-Out",
+    ["node.description"] = "Virtual Audio Cable (Output)",
+    ["media.class"] = "Audio/Sink",
+    ["audio.position"] = "FL,FR"
+  })
+
+  -- Create virtual source (input)
+  create_object("factory", {
+    ["factory.name"] = "support.null-audio-source",
+    ["node.name"] = "Virtual-Cable-In",
+    ["node.description"] = "Virtual Audio Cable (Input)",
+    ["media.class"] = "Audio/Source/Virtual",
+    ["audio.position"] = "FL,FR"
+  })
+'';
                 extraConfig.pipewire."91-null-sinks" = {
-                       "context.objects" = [
-    {
-      # A default dummy driver. This handles nodes marked with the "node.always-driver"
-      # properyty when no other driver is currently active. JACK clients need this.
-      factory = "spa-node-factory";
-      args = {
-        "factory.name"     = "support.node.driver";
-        "node.name"        = "Dummy-Driver";
-        "priority.driver"  = 8000;
-      };
-    }
-    {
-      factory = "adapter";
-      args = {
-        "factory.name"     = "support.null-audio-sink";
-        "node.name"        = "Microphone-Proxy";
-        "node.description" = "Microphone";
-        "media.class"      = "Audio/Source/Virtual";
-        "audio.position"   = "MONO";
-      };
-    }
-    {
-      factory = "adapter";
-      args = {
-        "factory.name"     = "support.null-audio-sink";
-        "node.name"        = "Main-Output-Proxy";
-        "node.description" = "Main Output";
-        "media.class"      = "Audio/Sink";
-        "audio.position"   = "FL,FR";
-      };
-    }
-        ]; 
+                        context.objects = [
+                                {
+                                        factory = "adapter";
+                                        args = {
+                                                "factory.name" = "support.null-audio-sink";
+                                                "node.name" = "Game-Audio";
+                                                "node.description" = "Game Audio";
+                                                "media.class" = "Audio/Sink";
+                                                "audio.position" = "FL,FR";
+                                        };
+                                }
+                        ];
                 };
         };
     flatpak = {
