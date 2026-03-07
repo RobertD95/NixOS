3684df9e310d3d8452b5b7009fabef296d272cf3
RobertD95 <robertparson95@protonmail.com>
2026-03-06 19:11:58 -0800

    Hypr Programs
    Removed from Home-Manager and added to nix config
    1. Hyprshot
    2. Hyprpaper
    3. Hyprsunset
    4. hyprpolkitagent

--- modules/packages/hyprland/default.nix
+++ modules/packages/hyprland/default.nix
@@ -1,15 +1,20 @@
 { pkgs, inputs, ... }:
-let
-  hypr = inputs.hyprland.packages.x86_64-linux;
-in
-{
-  # Hyprland
-  programs = {
-    hyprland = {
-      enable = true;
-      withUWSM = true;
-      package = hypr.hyprland;
-      portalPackage = hypr.xdg-desktop-portal-hyprland;
-    };
-  };
+        let
+                hypr = inputs.hyprland.packages.x86_64-linux;
+        in {
+        # Hyprland
+        programs = {
+                hyprland = {
+                enable = true;
+                withUWSM = true;
+                package = hypr.hyprland;
+                portalPackage = hypr.xdg-desktop-portal-hyprland;
+                };
+                hyprshot.enable = true;
+        };
+        services = {
+                hyprpaper.enable = true;
+                hyprsunset.enable = true;
+                hyprpolkitagend.enable = true;
+        };
 }
