f2013139b4d903759f1a4aa441deae86c6575e9c
RobertD95 <robertparson95@protonmail.com>
2026-03-06 20:52:56 -0800

    Update greeter.nix

--- modules/greeter.nix
+++ modules/greeter.nix
@@ -1,32 +1,31 @@
-{ pkgs, lib, inputs, ... }:
+{ pkgs, inputs, ... }:
 let
         hypr = inputs.hyprland.packages.x86_64-linux;
-in
-{
-  services.greetd = {
-    enable = true;
-    settings = {
-      initial_session = {
-        command = "${hypr.hyprland}/bin/Hyprland -q";
-        user = "robby";
-      };
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --asterisks --time --cmd '${hypr.hyprland}/bin/Hyprland'";
-        user = "greeter";
-      };
-    };
-    systemd.serviceConfig = {
-      Type = "idle";
-      StandardInput = "tty";
-      StandardOutput = "tty";
-      StandardError = "journal";
-      TTYReset = true;
-      TTYVHangup = true;
-      TTYVTDisallocate = true;
-      KeyringMode = lib.mkForce "inherit";
-    };
-  };
+        tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
+        hyprland-session = "${hypr.hyprland}/share/wayland-sessions";
+in {
+        services.greetd = {
+                enable = true;
+                        settings = {
+                                default_session = {
+                                        command = "${tuigreet} --time --remember --remember-session --sessions ${hyprland-session}";
+                                        user = "greeter";
+                                };
+                        };
+                };
 
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
-}   
+  # this is a life saver.
+  # literally no documentation about this anywhere.
+  # might be good to write about this...
+  # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
+}
