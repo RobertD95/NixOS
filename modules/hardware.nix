ee2e480300435ca9a05d42ac829d4ff0da911640
RobertD95 <robertparson95@protonmail.com>
2026-03-13 13:39:36 -0700

    Update hardware.nix
    hyprland's mesa are enabled

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -1,5 +1,6 @@
 { pkgs, inputs, ... }:
 let
+  hypr = inputs.hyprland.packages.x86_64-linux;
   nix = inputs.nix.legacyPackages.x86_64-linux;
   nyx = inputs.nyx.legacyPackages.x86_64-linux;
 in
@@ -8,9 +9,11 @@ in
   hardware = {
     graphics = {
       enable = true;
-      package = nyx.mesa_git;
+#package = nyx.mesa_git;
+      package = hypr.mesa;
       enable32Bit = true;
-      package32 = nyx.mesa32_git;
+#package32 = nyx.mesa32_git;
+      package32 = hypr.mesa;
     };
     amdgpu = {
       initrd.enable = true;
