a60479d52516f2cee479df065e3699f58aab33f4
RobertD95 <robertparson95@protonmail.com>
2026-03-13 13:42:25 -0700

    Update hardware.nix

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -1,24 +1,24 @@
 { pkgs, inputs, ... }:
 let
-  hypr = inputs.hyprland.packages.x86_64-linux;
-  nix = inputs.nix.legacyPackages.x86_64-linux;
-  nyx = inputs.nyx.legacyPackages.x86_64-linux;
+        hypr = inputs.hyprland.packages.x86_64-linux;
+        nix = inputs.nix.legacyPackages.x86_64-linux;
+        nyx = inputs.nyx.legacyPackages.x86_64-linux;
 in
 {
-  # GPU
-  hardware = {
-    graphics = {
-      enable = true;
-#package = nyx.mesa_git;
-      package = hypr.mesa;
-      enable32Bit = true;
-#package32 = nyx.mesa32_git;
-      package32 = hypr.mesa;
-    };
-    amdgpu = {
-      initrd.enable = true;
-      opencl.enable = true;
-    };
-  };
+        # GPU
+        hardware = {
+                graphics = {
+                        enable = true;
+                        #package = nyx.mesa_git;
+                        package = hypr.mesa;
+                        enable32Bit = true;
+                        #package32 = nyx.mesa32_git;
+                        package32 = hypr.pkgsi686Linux.mesa;
+                };
+                amdgpu = {
+                        initrd.enable = true;
+                        opencl.enable = true;
+                };
+        };
   hardware.bluetooth.enable = true; # Bluetooth
 }
