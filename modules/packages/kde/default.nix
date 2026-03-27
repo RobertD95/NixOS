ca7e9f874d17bcf80f66ebd221dc9db1e8bb4fdd
RobertD95 <robertparson95@protonmail.com>
2026-03-26 22:00:34 -0700

    htop-vim added

--- modules/packages/kde/default.nix
+++ modules/packages/kde/default.nix
@@ -3,8 +3,8 @@
         programs.kdeconnect.enable = true;
         services.desktopManager.plasma6.enable = true;
         environment.plasma6.excludePackages = [
-                pkgs.kdePackages.elisa
-                pkgs.kdePackages.konsole
+               pkgs.kdePackages.elisa
+               pkgs.kdePackages.konsole
         ];
         environment.systemPackages = with pkgs; [
                 kdePackages.ark
--- modules/packages/libdrm/default.nix
+++ modules/packages/libdrm/default.nix
@@ -1,12 +1,11 @@
 { pkgs, inputs, ... }:
 let
-  nix = inputs.nix.legacyPackages.x86_64-linux;
   nyx = inputs.nyx.legacyPackages.x86_64-linux;
 in
 {
-  environment.systemPackages = [
-    nyx.libdrm_git
-    nyx.libdrm32_git
+  environment.systemPackages = with nyx; [
+    libdrm_git
+    libdrm32_git
   ];
 }
 
